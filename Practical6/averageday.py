287b36e93dbf89a88ce91f2786c531733dd4e102
cg <guo.23@intl.zju.edu.cn>
2024-03-25 11:51:41 +0800

    activities

--- Practical6/averageday.py
+++ Practical6/averageday.py
@@ -4,15 +4,15 @@ c=3.5  #studying time
 d=2    #tv time 
 e=1    #music time
 f=24-a-b-c-d-e #f=other time
-dict={'sleeping':a,'C':b,'studying':c,'tv':d,'music':e,'other':f}
+dict={'sleeping':a,'C':b,'studying':c,'tv':d,'music':e,'other':f} #the dictionary
 print(dict)
 s='sleeping' #changeable
 activity=['sleeping','classes','studying','tv','music','other']
-if s in activity:
+if s in activity:   #examine whether there is the activity or not
     print(dict[s])
 else:
     print(dict['other'])
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #draw a pie chart
 time_day = [a,b,c,d,e,f]
 else1 = [0, 0, 0, 0, 0, 0]
 activity=['sleeping','classes','studying','tv','music','other']
