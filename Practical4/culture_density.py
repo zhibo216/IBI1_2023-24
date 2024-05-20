ded809b436ed4c8289b2ac3b4843571d909853d7
cg <guo.23@intl.zju.edu.cn>
2024-05-20 18:47:31 +0800

    culture_density

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -3,4 +3,5 @@ t=0 #the number of the day
 while a<=0.9: #density is lower than the objective '0.9'
     a=2*a
     t+=1  #next day
+    print('the cell density on the end of day'+str(t)+' is '+str(a))
 print('After day'+str(t-1)+' I can take a break') #I should leave the lab before the density is over '0.9'!
