7f1e1c3443ea868c83243513c8e400e6abb2a949
zhibo216 <156769714+zhibo216@users.noreply.github.com>
2024-05-22 02:23:23 +0800

    IBI1_student_database.py

--- Practical9/IBI1_student_database.py
+++ Practical9/IBI1_student_database.py
@@ -1,9 +1,15 @@
 class students: #create a class named students 
-    def __init__(self,n,m,c,g,e):
+    def __init__(self,n,m,c,g,e):  # the short names
+        if not (0 <=c<= 100):  
+            raise ValueError("Code Portfolio Score must be out of 100")  
+        if not (0 <=g<= 100):  
+            raise ValueError("Group Project Score must be out of 100.")  
+        if not (0 <=e<= 100):  
+            raise ValueError("Exam Score must be out of 100.") 
         self.name=n
         self.major=m
-        self.code_portifilo_score=c
+        self.code_portfolio_score=c
         self.group_project_score=g
         self.exam_score=e
-x=students('Jasonona','BMI',100,100,100)
-print(x.name,x.major,x.code_portifilo_score,x.group_project_score,x.exam_score)
+x=students('Jasonona','BMI',100,100,100)  #name,major,code_portfolio score,group_project score, exam score
+print(x.name,x.major,x.code_portfolio_score,x.group_project_score,x.exam_score)
